4f7bd435ea2e1a7206531df831b5616277f6e859
MFarouk11 <mohamed.aboelfetouh@ejust.edu.eg>
2025-09-05 22:22:54 +0300

    Added image loading with OpenCV

---
+++ main.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt 
+
+# Load the image
+image_path = "C:/Users/Farouk/Downloads/test.jpg"  # Replace with your image path
+image = cv2.imread(image_path)
