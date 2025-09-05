f593b3e177cf1d0ade662d721ac9214e43a84259
MFarouk11 <mohamed.aboelfetouh@ejust.edu.eg>
2025-09-05 22:26:23 +0300

    Added HSV color detection function

--- main.py
+++ main.py
@@ -18,3 +18,22 @@ edges = cv2.Canny(blurred, 45, 140)
 contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 output_image = image_rgb.copy()
 
+# HSV color detection function
+def identify_color_hsv(bgr_color):
+    # BGR to HSV
+    hsv_color = cv2.cvtColor(np.uint8([[bgr_color]]), cv2.COLOR_BGR2HSV)[0][0]
+    h, s, v = hsv_color
+
+    # rnges of colors
+    if s < 50 and v > 200:
+        return "white"
+    elif h < 10 or h > 160:
+        return "red"
+    elif 20 < h < 40:  
+        return "yellow"
+    elif 40 < h < 80:
+        return "green"
+    elif 80 < h < 140:
+        return "blue"
+    else:
+        return "unknown"
