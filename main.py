dc9b02ad39ab8ef6a05e9e9177ea3a3a9fb3423b
MFarouk11 <mohamed.aboelfetouh@ejust.edu.eg>
2025-09-05 22:25:06 +0300

    Converted image to RGB and Grayscale

--- main.py
+++ main.py
@@ -3,5 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 # Load the image
-image_path = "C:/Users/Farouk/Downloads/test.jpg"  # Replace with your image path
+image_path = "C:/Users/Farouk/Downloads/test.jpg" 
 image = cv2.imread(image_path)
+
+
+
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+# Edge detection
+blurred = cv2.GaussianBlur(image_gray, (5, 5), 0)
+edges = cv2.Canny(blurred, 45, 140)
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+output_image = image_rgb.copy()
+
